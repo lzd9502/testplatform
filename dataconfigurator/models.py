d37c63d0d61abd1e7ef7e73fc717632445c563ed
lzd9502 <46022547+lzd9502@users.noreply.github.com>
2019-05-30 18:33:32 +0800

    1.1

--- dataconfigurator/models.py
+++ dataconfigurator/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from tester.models import case
 
 # Create your models here.
-class datasource(models.Model):
-    type_choice=[(0,'sql'),(1,'fix'),(2,'function')]
-    name=models.CharField(max_length=16,verbose_name='源名')
-    sourcetype=models.CharField(choices=type_choice,default=1,verbose_name='数据源形式')
-    value=models.TextField(max_length=500,verbose_name='源值')
+class DataSource(models.Model):
+    type_choice = [(0, 'sql'), (1, 'fix'), (2, 'function')]
+    name = models.CharField(max_length=16, verbose_name='源名')
+    source_type = models.CharField(max_length=1,choices=type_choice, default=1, verbose_name='数据源形式')
+    source_value_runner = models.TextField(max_length=500, verbose_name='源数据获取器')
 
-    pass
-class case2source(models.Model):
 
-    case=models.ForeignKey(case,on_delete=models.CASCADE)
-    datasource=models.ForeignKey(datasource,on_delete=models.CASCADE)
-
+class SourceResult(models.Model):
+    name = models.CharField(max_length=16, verbose_name='结果名')
+    datasource = models.ForeignKey(DataSource, on_delete=models.CASCADE, related_name='children')
--- tester/models.py
+++ tester/models.py
@@ -1,45 +1,65 @@
 from django.db import models
 from testenvconfig.models import Project
+from dataconfigurator.models import SourceResult
 from django.contrib.auth import get_user_model
 
-user=get_user_model()
+user = get_user_model()
+
+
 # Create your models here.
-class route(models.Model):
+class Route(models.Model):
     '''
 
     '''
     name = models.CharField(max_length=16, verbose_name='routeName')
     route = models.CharField(max_length=50, verbose_name='route')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE,related_name='myroute')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='myroute')
     pass
 
 
-class routeParams(models.Model):
-    type_choice = [(0, 'header'), (1, 'param'), (2, 'body'), (3, 'urlparam')]
+class RouteParams(models.Model):
+    type_choice = [(0, 'header'), (1, 'param'), (2, 'body'), (3, 'url_param')]
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, verbose_name='所属路由', related_name='myrouteparams')
     param = models.CharField(max_length=16, verbose_name='参数名')
-    datatype = models.CharField(choices=type_choice, default=1, verbose_name='参数形式')
-    route = models.ForeignKey(route, on_delete=models.CASCADE, verbose_name='所属路由',related_name='myrouteparams')
+    datatype = models.CharField(max_length=1, choices=type_choice, default=1, verbose_name='参数形式')
 
 
-class routeResponseGroup(models.Model):
+class RouteResponseGroup(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, verbose_name='所属路由', related_name='myresponsegroup')
     name = models.CharField(max_length=16, verbose_name='responseGroupName')
-    route = models.ForeignKey(route, on_delete=models.CASCADE, verbose_name='所属路由',related_name='myresponsegroup')
 
 
 class ResponseGroupParam(models.Model):
+    Group = models.ForeignKey(RouteResponseGroup, on_delete=models.CASCADE, related_name='mygroupparams')
     param = models.CharField(max_length=16, verbose_name='参数名')
-    Group = models.ForeignKey(routeResponseGroup, on_delete=models.CASCADE,related_name='mygroupparams')
 
 
-class case(models.Model):
-    name=models.CharField(max_length=16,verbose_name='用例名')
-    route=models.ForeignKey(route,on_delete=models.SET_NULL,related_name='mycase')
-    createtime=models.DateTimeField(auto_now_add=True)
-    createby=models.ForeignKey(user,on_delete=models.SET_NULL)
-    updatetime=models.DateTimeField(auto_now=True)
-    lastupdateby=models.ForeignKey(user,on_delete=models.SET_NULL)
+class Case(models.Model):
+    case_method_choice = [('GET','get'), ('POST','post'), ('PUT','put'), ('DELETE','delete')]
+    name = models.CharField(max_length=16, verbose_name='用例名')
+    req_method = models.CharField(max_length=6, choices=case_method_choice, default=case_method_choice[0])
+    createtime = models.DateTimeField(auto_now_add=True)
+    createby = models.ForeignKey(user, null=True, on_delete=models.SET_NULL, related_name='casecreater')
+    updatetime = models.DateTimeField(auto_now=True)
+    updateby = models.ForeignKey(user, null=True, on_delete=models.SET_NULL, related_name='caseupdater')
     pass
 
 
-class result(models.Model):
-    pass
+class Case_Source_RouteParam(models.Model):
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, related_name='myCSRP')
+    param = models.ForeignKey(RouteParams, null=True, on_delete=models.SET_NULL)
+    value = models.ForeignKey(SourceResult, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        unique_together = ('case', 'param', 'value')
+
+
+class Case_Source_RouteResponse(models.Model):
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, related_name='myCSRR')
+    param = models.ForeignKey(RouteParams, null=True, on_delete=models.SET_NULL)
+    value = models.ForeignKey(SourceResult, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        unique_together = ('case', 'param', 'value')
+# class result(models.Model):
+#     pass
